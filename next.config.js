1bcfa64d0e6ca2a77674bc0fa07c5dbc7ca8061f
Sayyed Arslaan <arslaan.developer@gmail.com>
2025-08-21 23:38:25 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
-        pathname: "/dccm287b8/**",
+        pathname: "/dccm287b8/image/upload/Arslaan/**",
         search: "",
       },
     ],
