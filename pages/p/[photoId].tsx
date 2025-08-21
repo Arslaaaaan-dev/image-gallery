6338aceb3a6b529b225af26d1ada5857252f34be
Sayyed Arslaan <arslaan.developer@gmail.com>
2025-08-21 12:15:46 +0530

    Update [photoId].tsx

--- pages/p/[photoId].tsx
+++ pages/p/[photoId].tsx
@@ -17,7 +17,7 @@ const Home: NextPage = ({ currentPhoto }: { currentPhoto: ImageProps }) => {
   return (
     <>
       <Head>
-        <title>Next.js Conf 2022 Photos</title>
+        <title>Arslaan gallery Next.js 2025</title>
         <meta property="og:image" content={currentPhotoUrl} />
         <meta name="twitter:image" content={currentPhotoUrl} />
       </Head>
