a199174953fd2f0719e0dd49935a158e2adb2457
Sayyed Arslaan <arslaan.developer@gmail.com>
2025-08-22 15:28:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/dccm287b8/image/upload/Arslaan/**",
-        search: "",
+        protocol: "https",         // Keep this as is - always use HTTPS
+        hostname: "res.cloudinary.com",  // Keep this as is - this is Cloudinary's domain
+        port: "",                  // Keep empty
+        pathname: `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/**`,  // This is the important change
+        search: "",                // Keep empty
       },
     ],
   },
